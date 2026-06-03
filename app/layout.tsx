fc793af48aa58d70eb172f82e505b87cc79e1c73
Sebastian Larsen <sebastianmlarsen@outlook.com>
2026-06-03 08:22:31 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import Script from "next/script"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +48,7 @@ export default function RootLayout({
 </Script>
 
 {children}
+<Analytics />
         {children}
       </body>
     </html>
